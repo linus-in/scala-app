9a991fccb7e41c002cc0c2887bd4a0e5167cf474
linus-in <offgit.ot@gmail.com>
2020-01-16 22:30:12 +0530

    Added implicit ordering for employee

--- src/sample/scala/app/implicitex/ImplicitExamples2.scala
+++ src/sample/scala/app/implicitex/ImplicitExamples2.scala
@@ -1,5 +1,5 @@
 package sample.scala.app.implicitex
-
+//implicits can't be defined at top level..it can only be defined in class, object ,trait
 object ImplicitExamples2 extends App {
   
   //value mod is not a member of Int
@@ -16,5 +16,20 @@ object ImplicitExamples2 extends App {
   //println(5 mod 2)
   implicit def getMathOps(num:Int):MathOps=MathOps(num)
   println(5 mod 2)//Searches for all implicits(class,method,val,etc) to fit the call
+  
+  //Implicit ordering for class Employee
+  
+  case class Employee(name:String,id:Int)
+  val li=List(2,1,4,3)
+  //li.sorted
+  
+  val empl=List(Employee("A",1),Employee("B",2),Employee("D",4),Employee("C",3))
+  //No implicit Ordering defined for sample.scala.app.implicitex.ImplicitExamples2.Employee.
+  //empl.sorted
+  //implicit def employeeOrdering:Ordering[Employee]=Ordering.--doubtful
+  implicit object EmployeeNameOrdering extends Ordering[Employee]{
+    def compare(a:Employee,b:Employee)=a.name compare b.name
+  }
+  println(empl.sorted)
   
 }
